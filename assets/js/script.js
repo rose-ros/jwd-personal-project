2872cbe90408fdc2f87ee56d9e712ae1f1c76f37
Rosemarie Rosario <rosariorosemarie@gmail.com>
2023-07-17 22:23:20 -0700

    added img upload feature and hid buttons on change

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,23 @@
-const generateColor = document.querySelector('#gen-color');
+const genColor = document.querySelector(".gen-color");
+const profilePic = document.getElementById("profile-pic");
+const upload = document.getElementById("upload");
+const hex = document.getElementById("hex");
+
+
+// uploads image
+upload.addEventListener('change', () => {
+    profilePic.src = URL.createObjectURL(upload.files[0]);
+    // Hides upload btn if img is uploaded, hides generate & hex w/o img
+    if (upload.files && upload.files[0]) {
+        const uploadBtn = document.getElementById("upload-btn");
+        uploadBtn.style.display = "none";
+        genColor.classList.remove("hide");
+        hex.classList.remove("hide");
+    } else {
+        uploadBtn.style.display = "block";
+        genColor.classList.add("hide");
+        hex.classList.add("hide");
+    }
+});
+
+
