82f01190a7550e5486bc5ba697f7351ee7137b31
Rosemarie Rosario <rosariorosemarie@gmail.com>
2023-07-17 23:51:19 -0700

    responsive image

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,16 +8,14 @@ const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
 // uploads image
 upload.addEventListener('change', () => {
     profilePic.src = URL.createObjectURL(upload.files[0]);
-    // Hides upload btn if img is uploaded, hides generate & hex w/o img
+    // Hides upload btn if img is uploaded, hides generate w/o img
     if (upload.files && upload.files[0]) {
         const uploadBtn = document.getElementById("upload-btn");
         uploadBtn.style.display = "none";
         genColor.classList.remove("hide");
-        hexCode.classList.remove("hide");
     } else {
         uploadBtn.style.display = "block";
         genColor.classList.add("hide");
-        hexCode.classList.add("hide");
     }
 });
 
