3c8a787bfdbdf9442c23ba1763b873b5c7705d66
Rosemarie Rosario <rosariorosemarie@gmail.com>
2023-07-17 22:42:09 -0700

    created random hex bg on click

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,8 @@
 const genColor = document.querySelector(".gen-color");
 const profilePic = document.getElementById("profile-pic");
 const upload = document.getElementById("upload");
-const hex = document.getElementById("hex");
+const hexCode = document.getElementById("hex");
+const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
 
 
 // uploads image
@@ -12,12 +13,24 @@ upload.addEventListener('change', () => {
         const uploadBtn = document.getElementById("upload-btn");
         uploadBtn.style.display = "none";
         genColor.classList.remove("hide");
-        hex.classList.remove("hide");
+        hexCode.classList.remove("hide");
     } else {
         uploadBtn.style.display = "block";
         genColor.classList.add("hide");
-        hex.classList.add("hide");
+        hexCode.classList.add("hide");
     }
 });
 
+// generates random hex on click
+genColor.addEventListener('click', () => {
+    let color = "#";
+    for (let i = 0; i < 6; i++) {
+        color += hex[getRandomNumber()];
+    }
+    hexCode.innerHTML = color;
+    document.querySelector(".bg-color").style.backgroundColor = color;
+});
 
+function getRandomNumber() {
+    return Math.floor(Math.random() * hex.length);
+}
