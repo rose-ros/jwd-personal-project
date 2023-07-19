866e6b5ea7a37e53e2f73bd5ff487024f5cb63ef
Rosemarie Rosario <rosariorosemarie@gmail.com>
2023-07-19 11:27:00 -0700

    finished responsive navbar

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,48 +1,79 @@
-const genColor = document.querySelector(".gen-color");
-const profilePic = document.getElementById("profile-pic");
+// navbar selectors
+const navbar = document.querySelector('.navbar');
+const historyTab = document.querySelector('.history-tab');
+const historyItems = document.querySelector('.history-dropdown');
+const favoritesTab = document.querySelector('.favorites-tab');
+const favoritesItems = document.querySelector('.favorites-dropdown');
+const navToggle = document.querySelector('.navbar-toggle');
+const navItems = document.querySelector('.nav-items');
+const content = document.querySelector('.content');
+
+// event handlers
+const displayBlock = (item) => item.style.display = 'block';
+const displayFlex = (item) => item.style.display = 'flex';
+const displayNone = (item) => item.style.display = 'none';
+
+// show tabs on mouseover
+historyTab.addEventListener('mouseover', () => displayFlex(historyItems));
+favoritesTab.addEventListener('mouseover', () => displayFlex(favoritesItems));
+
+// hide tabs on mouseout
+navbar.addEventListener('mouseout', () => {
+    displayNone(historyItems);
+    displayNone(favoritesItems);  
+});
+
+// small screen nav toggle
+navToggle.addEventListener('click', () => {
+    navItems.style.display = navItems.style.display === 'none' ? 'flex' : 'none';
+});
+
+// small screen hide nav when mouseover outside content
+content.addEventListener('mouseover', () => {
+    if (getComputedStyle(navToggle).display === 'block') {
+        displayNone(navItems);
+    }
+});  
+
+// Displays nav items on window resize
+function reloadNavItems() {
+    if (window.innerWidth > 600) {
+        displayFlex(navItems);
+    } else {
+        displayNone(navItems);
+    }
+}
+
+window.addEventListener('resize', reloadNavItems);
+
+
+
 const upload = document.getElementById("upload");
+const profilePic = document.getElementById("profile-pic");
+const infoBox = document.querySelector('.info-box');
+const genColor = document.querySelector(".gen-color");
 const hexCode = document.getElementById("hex");
 const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
-
-// navbar
-const navbar = document.querySelector('.navbar');
-
-const historyTab = document.querySelector('.history-toggle');
-const historyItems = document.querySelector('.history-items');
-
-const favoritesTab = document.querySelector('.favorites-toggle');
-const favoritesItems = document.querySelector('.favorites-items');
-
-historyTab.addEventListener('mouseover', () => {
-    historyItems.style.display = 'flex';
-});
-
-navbar.addEventListener('mouseout', () => {
-    historyItems.style.display = 'none';
-});
-
-favoritesTab.addEventListener('mouseover', () => {
-    favoritesItems.style.display = 'flex';
-});
-  
-navbar.addEventListener('mouseout', () => {
-    favoritesItems.style.display = 'none';
-});
-
 
 // uploads image
 upload.addEventListener('change', () => {
     profilePic.src = URL.createObjectURL(upload.files[0]);
+    const uploadBtn = document.getElementById("upload-btn");
     // Hides upload btn if img is uploaded, hides generate w/o img
     if (upload.files && upload.files[0]) {
-        const uploadBtn = document.getElementById("upload-btn");
-        uploadBtn.style.display = "none";
-        genColor.classList.remove("hide");
+        displayNone(uploadBtn);
+        displayBlock(genColor);
+        displayFlex(infoBox);
     } else {
-        uploadBtn.style.display = "block";
-        genColor.classList.add("hide");
+        displayBlock(uploadBtn);
+        displayNone(genColor);
+        displayNone(infoBox);
     }
 });
+
+function getRandomNumber() {
+    return Math.floor(Math.random() * hex.length);
+}
 
 // generates random hex on click
 genColor.addEventListener('click', () => {
@@ -53,7 +84,3 @@ genColor.addEventListener('click', () => {
     hexCode.innerHTML = color;
     document.querySelector(".bg-color").style.backgroundColor = color;
 });
-
-function getRandomNumber() {
-    return Math.floor(Math.random() * hex.length);
-}
